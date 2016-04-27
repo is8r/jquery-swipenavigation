ff8f4152a77f69b4841ec299019147af238c1ee7
Yu Ishihara <ishihara.yu@gmail.com>
2016-04-27 19:05:22 +0900

    fix jquery plugin

--- source/javascripts/jquery.swipenavigation.js
+++ source/javascripts/jquery.swipenavigation.js
@@ -22,29 +22,34 @@
     plugin.init = function() {
         plugin.settings = $.extend({}, defaults, options);
         plugin.initListner();
-        plugin.initHover();
     },
 
     //----------------------------------------------------------------------
 
     plugin.initListner = function(e) {
-      $(element).on('mousewheel', function(event) {
-        event.preventDefault();
-        var $el = $(element).find('ul');
-        $el.scrollLeft($el.scrollLeft()-event.deltaY);
-      });
-    },
 
-    plugin.initHover = function(e) {
-      $(element).find('a').each(function(index, el) {
-        var $el = $(el);
-        $el.on('mouseover touchstart', function(event) {
-          $el.addClass('hover');
+      var $target = $(element);
+      var hasTapEvent = ('ontouchstart' in window);
+
+      if(hasTapEvent) {
+        $target.find('a').each(function(index, el) {
+          var $el = $(el);
+          $el.addClass('hover-disabled');
+          $el.on('touchstart', function(event) {
+            $el.addClass('touch');
+          });
+          $el.on('touchend', function(event) {
+            $el.removeClass('touch');
+          });
         });
-        $el.on('mouseout touchend', function(event) {
-          $el.removeClass('hover');
+      } else {
+        $target.on('mousewheel', function(event) {
+          event.preventDefault();
+          var $el = $target.find('ul');
+          $el.scrollLeft($el.scrollLeft()-event.deltaY);
         });
-      });
+      }
+
     },
 
     //----------------------------------------------------------------------
