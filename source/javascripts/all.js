7dc39138c4792666f97e6ad2d91ef081ff679f29
Yu Ishihara <ishihara.yu@gmail.com>
2016-04-27 18:59:33 +0900

    refactoring

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,27 +1,10 @@
 // This is where it all goes :)
 
-var _ua = (function(u){
-  return {
-    Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1)
-      || u.indexOf("ipad") != -1
-      || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
-      || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
-      || u.indexOf("kindle") != -1
-      || u.indexOf("silk") != -1
-      || u.indexOf("playbook") != -1,
-    Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
-      || u.indexOf("iphone") != -1
-      || u.indexOf("ipod") != -1
-      || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
-      || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
-      || u.indexOf("blackberry") != -1
-  }
-})(window.navigator.userAgent.toLowerCase());
-
 $(document).on('ready', function() {
   var $target = $('.js-swipenavigation');
+  var hasTapEvent = ('ontouchstart' in window);
 
-  if(_ua.Tablet || _ua.Mobile) {
+  if(hasTapEvent) {
     $target.find('a').each(function(index, el) {
       var $el = $(el);
       $el.addClass('hover-disabled');
